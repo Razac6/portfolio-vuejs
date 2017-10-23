f77f991c7ea4382ed9388729e11b7a5a542f287e
Marcin Binek <marcin.binek@summer.agency>
2017-10-23 14:53:07 +0200

    refactor

---
+++ src/components/home/home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <section class="home">
+      <div class="row-wbd">
+        <div class="header">Front End Developer</div>
+        <div class="lead">Jan Kowalski</div>
+        {{message}}
+        <mouse-scroll></mouse-scroll>
+      </div>
+    </section>
+     <about></about> 
+    <skills></skills>
+    <app-footer></app-footer>
+  </div>
+</template>
+<script>
+
+import About from '../about/about.vue';
+import Skills from '../skills/skills.vue';
+import AppFooter from '../footer/footer.vue';
+import MouseScroll from '../share/mouse-scroll/mouse-scroll.vue';
+export default {
+  components: {
+    About,
+    Skills,
+    AppFooter,
+    MouseScroll
+  },
+  name: "home",
+};
+</script>
+<style lang="scss" scoped>
+section {
+  &.home {
+    background-color: #efcb7b;
+  }
+  width: 100%;
+  height: 100vh;
+  color: #fff;
+}
+.row-wbd{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  color: #24262d;
+
+  & .header{
+    font-size: 3rem;
+  }
+
+  & .lead{
+    font-size: 2rem;
+    opacity: 0.5;
+    font-weight: 100;
+  }
+
+ 
+
+}
+</style>
